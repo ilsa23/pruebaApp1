d098de78f64fd0d30718ef6c9d1972fa2ec7f809
Ilse Lopez <ilze.sarai@gmail.com>
2017-12-29 17:35:52 -0600

    Prueba1

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'prueba1',
+		wp_ws_url : 'http://localhost/wordpress/wp-appkit-api/prueba1',
+		wp_url : 'http://localhost/wordpress',
+		theme : 'q-android',
+		version : '1.0.0',
+		app_title : 'Prueba1',
+		app_platform : 'android',
+		gmt_offset : -6,
+		debug_mode : 'off',
+		auth_key : 'pon aquí tu frase aleatoria',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
